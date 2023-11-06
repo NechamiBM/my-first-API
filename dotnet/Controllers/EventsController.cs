e2eb101307d25e1657f645155b29b68d7c07376f
NechamiBM <149536574+NechamiBM@users.noreply.github.com>
2023-11-06 23:34:45 +0200

    Add files via upload

---
+++ dotnet/Controllers/EventsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace dotnet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventsController : ControllerBase
+    {
+        List<Event> EventList = new List<Event> { new Event(DateTime.Now), new Event(DateTime.Now) };
+
+        // GET: api/<EventsController>
+        [HttpGet]
+        public IEnumerable<Event> Get() => EventList;
+
+        // POST api/<EventsController>
+        [HttpPost]
+        public void Post([FromBody] string value) => EventList.Add(new Event { Title = value });
+
+
+        // PUT api/<EventsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value) => EventList.Find(c => c.Id == id).Title = value;
+
+
+        // DELETE api/<EventsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id) => EventList.Remove(EventList.Find(c => c.Id == id));
+        
+    }
+}
---
+++ dotnet/Event.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace dotnet
+{
+    public class Event
+    {
+        static int Counter = 0;
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public Event() : this(DateTime.Now) { }
+        public Event(DateTime date)
+        {
+            Id = Counter++;
+            Start = date;
+            Title = "sample event";
+        }
+    }
+}
